862941fe38db70a84bfeefc70dbd151072a4cce8
Daniel <152278243+dinpel@users.noreply.github.com>
2023-11-29 13:27:27 +0100

    Add files via upload

--- dip_ws2812.h
+++ dip_ws2812.h
@@ -23,14 +23,14 @@
 class WS2812 {
 private:
   const uint32_t *ramled,*ramtable;
-  uint8_t T0H,T1H,T0L,T1L,CH_RMT_TX,GPIO; //static_cast<rmt_channel_t>(1), static_cast<gpio_num_t>(4)
+  uint32_t RESET,ZERO,ONE;
+  uint8_t CH_RMT_TX,GPIO; //static_cast<rmt_channel_t>(1), static_cast<gpio_num_t>(4)
 void show(){
   rmt_wait_tx_done(static_cast<rmt_channel_t>(CH_RMT_TX), portMAX_DELAY);
   uint32_t *table=(uint32_t *)ramtable,*dest=(uint32_t *)ramled; 
-  rmt_item32_t rmt_tmp;
-  *dest++=(((1<<1)|1)<<16)|(2000<<1); //reset 50 microseg.
-  for (uint32_t led = 0;led<n_leds;led++) {  uint32_t be=*(table+led),m=0x800000; 
-    while (m) { rmt_tmp = (be&m) ? (rmt_item32_t){{{T1H, 1, T1L, 0}}} : (rmt_item32_t){{{T0H, 1, T0L, 0}}}; *dest++=rmt_tmp.val; m>>=1;} }
+  *dest++=RESET; for (uint32_t be,m,i=0;i<n_leds;i++) {  //reset 50 microseg.
+    be=*table++; m=0x800000;  while (m) { *dest++=(be&m)?ONE:ZERO; m>>=1;} 
+  }
   rmt_write_items(static_cast<rmt_channel_t>(CH_RMT_TX),(const rmt_item32_t*)ramled,((n_leds*24)+1), false);
 }
 public:
@@ -42,16 +42,19 @@ void rotate(int8_t pasos=1){
   for(int j=0;j<pasos;j++) { uint32_t *m=(uint32_t *)ramtable,pe=*m;for(int i=0;i<n_leds-1;i++){ *(m+i)=*(m+i+1); } *(m+n_leds-1)=pe;}
   show();
 }
-void clear(uint32_t vl=0){ for(int i=0;i<n_leds;i++) led(i,vl); show(); } // off all leds
-void times(uint32_t nt0h,uint32_t nt0l,uint32_t nt1h,uint32_t nt1l){ T0H=nt0h/25; T0L=nt0l/25; T1H=nt1h/25;  T1L=nt1l/25; }
+void clear(uint32_t vl=0){ for(int i=0;i<n_leds;i++) rgb(i,(vl&0xff0000)>>16,(vl&0xff00)>>8,vl); show(); } // off all leds
+void times(uint32_t t0h,uint32_t t0l,uint32_t t1h,uint32_t t1l){ 
+  RESET=(rmt_item32_t){1,1,2000, 0}.val; 
+  ZERO=(rmt_item32_t){t0h/25,1,t0l/25,0}.val; ONE=(rmt_item32_t){t1h/25,1,t1l/25,0}.val; 
+  }
 void rgb(uint8_t nl,uint8_t r,uint8_t g,uint8_t b,bool showf=false){ if (nl<n_leds) { *((uint32_t *)ramtable+nl)=g<<16|r<<8|b; if (showf) show(); } }
 void led(uint8_t nl,uint32_t vl,bool showf=false){  if (nl<n_leds) rgb(nl,vl>>16,vl>>8,vl,showf); }
 void begin(uint8_t leds,uint8_t can,uint8_t port){
   n_leds=leds; ramled=(uint32_t*)malloc((leds*24*4)+4); ramtable=(uint32_t*)malloc(leds*4);
-  T0H=(400/25); T0L=(850/25); T1H=(800/25); T1L=(450/25); CH_RMT_TX=can; GPIO=port;
-  rmt_config_t config = {.rmt_mode = RMT_MODE_TX,.channel = static_cast<rmt_channel_t>(CH_RMT_TX),.gpio_num = static_cast<gpio_num_t>(GPIO),.mem_block_num = 3 }; 
-  config.clk_div = 2; /* 80Mhz/2,step=25nseg */ rmt_config(&config); rmt_driver_install(config.channel, 0, 0); clear(); 
+  times(400,850,800,450); CH_RMT_TX=can; GPIO=port;
+  rmt_config_t config = {.rmt_mode = RMT_MODE_TX,.channel = static_cast<rmt_channel_t>(CH_RMT_TX),.gpio_num = static_cast<gpio_num_t>(GPIO),.clk_div=2,.mem_block_num=3 }; // clk_div 80Mhz/2,step=25nseg  
+  rmt_config(&config); rmt_driver_install(config.channel, 0, 0); clear(); 
 }
-} ws2812b;
+};
  
 #endif
--- dip_ws2812.ino
+++ dip_ws2812.ino
@@ -1,29 +1,39 @@
  
 #include "dip_ws2812.h"
+WS2812 l12,l16; 
 
 void setup(){
-ws2812b.begin(16,0,4); //number leds,channel rmt, gpio
+l16.begin(16,0,15);  
+l12.begin(12,3,16);  
 }
-void loop()
-{
-ws2812b.led(0,0); 
-ws2812b.rgb(1,70,0,0); ws2812b.rgb(2,70,0,0);  ws2812b.rgb(3,70,0,0); //led position,red,green,blue 
-ws2812b.led(4,0); 
-ws2812b.rgb(5,0,50,0); ws2812b.rgb(6,0,50,0);  ws2812b.rgb(7,0,50,0); 
-ws2812b.led(8,0); 
-ws2812b.rgb(9,0,0,90); ws2812b.rgb(10,0,0,90); ws2812b.rgb(11,0,0,90); 
-ws2812b.led(12,0); 
-ws2812b.rgb(13,0x20,0x20,0); ws2812b.rgb(14,0x20,0x20,0); ws2812b.led(15,0x202000,true);  //last optional parameter showf = true for show
+
+void loop(){
+
+l12.clear(0x330000); l16.clear(0xFF0000); delay(500); 
+l12.clear(0x3300); l16.clear(0xFF00); delay(500); 
+l12.clear(0x33); l16.clear(0xFF); delay(500); 
+
+l12.clear(); for(int j=0;j<l12.n_leds;j+=3) l12.led(j,0x111100,true);
+ 
+
+l16.led(0,0); 
+l16.rgb(1,70,0,0); l16.rgb(2,70,0,0);  l16.rgb(3,70,0,0); //led position,red,green,blue 
+l16.led(4,0); 
+l16.rgb(5,0,50,0); l16.rgb(6,0,50,0);  l16.rgb(7,0,50,0); 
+l16.led(8,0); 
+l16.rgb(9,0,0,90); l16.rgb(10,0,0,90); l16.rgb(11,0,0,90); 
+l16.led(12,0); 
+l16.rgb(13,0x20,0x20,0); l16.rgb(14,0x20,0x20,0); l16.led(15,0x202000,true);  //last optional parameter showf = true for show
 for(int j=0;j<2;j++){ 
-  for(int i=0;i<50;i++){ delay(125); ws2812b.rotate(1); }
-  for(int i=0;i<50;i++){ delay(40); ws2812b.rotate(-1); }
+  for(int i=0;i<50;i++){ delay(125); l16.rotate(1); l12.rotate(1); }
+  for(int i=0;i<50;i++){ delay(40); l16.rotate(-1); l12.rotate(-1); }
 }  
 for(int i=0;i<2;i++){
-  for(int j=0;j<ws2812b.n_leds;j++){ ws2812b.led(j,0xff0000,true); delay(100); }  ws2812b.clear(); delay(250);
-  for(int j=ws2812b.n_leds-1;j>=0;j--){ ws2812b.led(j,0x0000ff,true); delay(100); } ws2812b.clear(); delay(250);
+  for(int j=0;j<l16.n_leds;j++){ l16.led(j,0xff0000,true); delay(100); l12.rotate(1); }  l16.clear(); delay(250);
+  for(int j=l16.n_leds-1;j>=0;j--){ l16.led(j,0x0000ff,true); delay(100); l12.rotate(-1); } l16.clear(); delay(250);
 }
-for(int i=0;i<20;i++){
-  ws2812b.clear(0xff00); delay(50); ws2812b.clear(); delay(50);
+for(int i=0;i<5;i++){
+  l16.clear(0xFF0000); l12.clear(); delay(500); l16.clear(); l12.clear(0xFF0000); delay(500);
 }
 
 }
